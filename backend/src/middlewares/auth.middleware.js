8e9ec087e03cc35e2cc1f6239a2ec0fc335dbc51
lordxsumit <sum56it@gmail.com>
2026-09-10 18:00:08 +0530

    Added optional authentication and admin authentication functionality and made changes to the gitignore file

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import { AsyncHandler } from '../utils/AsyncHandler.js';
 
 
 // Validating the token from the user with the secret present in the server and then checking if any user with same signature is present or not.
-export const verifyJWT = AsyncHandler(async (req, res, next) => {
+const verifyJWT = AsyncHandler(async (req, res, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         if(!token){
@@ -24,4 +24,36 @@ export const verifyJWT = AsyncHandler(async (req, res, next) => {
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid access token")
     }
-})
+});
+
+const optionalAuth = AsyncHandler(async (req, res, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    if(!token){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    if(token){
+        try {
+            const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+            req.newUser = await user.findById(decodedToken._id).select("-password")
+        } catch (error) {
+            // ignore invalid token for optional auth
+        }
+    }
+    next()
+});
+
+const admin = AsyncHandler((req, res, next) => {
+  if (req.newUser && req.newUser.role === 'admin') {
+    next();
+  } else {
+    throw new ApiError(403, 'Not authorized as an admin');
+  }
+});
+
+
+export {
+    verifyJWT,
+    optionalAuth,
+    admin
+}
